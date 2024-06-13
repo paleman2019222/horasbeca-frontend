e6881188ddcbf8364fdcf7a85f6dfee0f433c3c7
paleman2019222 <61806840+paleman2019222@users.noreply.github.com>
2024-06-13 15:42:46 -0600

    creacion de frontend, componentes home, login y navbar

---
+++ horas-beca-frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ horas-beca-frontend/src/app/services/global.ts
@@ -0,0 +1,3 @@
+export const CONNECTION = {
+    URI: 'https://horasbeca-backend.onrender.com/api/'
+}
