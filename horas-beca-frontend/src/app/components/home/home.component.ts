682d15cfad7a88b52e974e8973d16f1123d39b79
paleman2019222 <61806840+paleman2019222@users.noreply.github.com>
2024-06-13 22:17:44 -0600

    delete activity

--- horas-beca-frontend/src/app/components/home/home.component.ts
+++ horas-beca-frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestUserService } from '../../services/rest-user.service';
+import { RestActivityService } from '../../services/rest-activity.service'; // Asegúrate de importar el servicio
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,17 @@ import { RestUserService } from '../../services/rest-user.service';
 export class HomeComponent implements OnInit {
   public activities: any[] = [];
   public allactivities: any[] = [];
-  public user: any="";
+  public user: any = "";
 
-  constructor(private restUserService: RestUserService) { }
+  constructor(
+    private restUserService: RestUserService,
+    private restActivityService: RestActivityService // Asegúrate de inyectar el servicio
+  ) {}
 
   ngOnInit(): void {
-     this.user = this.restUserService.getUser();
-   
-      this.loadUserActivities();
-      this.AllUserActivities();
+    this.user = this.restUserService.getUser();
+    this.loadUserActivities();
+    this.loadAllActivities();
   }
 
   loadUserActivities(): void {
@@ -30,18 +33,28 @@ export class HomeComponent implements OnInit {
       }
     );
   }
-  //Ver todas las actividades como administrador
-  AllUserActivities(): void {
+
+  loadAllActivities(): void {
     this.restUserService.getUserAllActivities(this.user._id).subscribe(
       (response) => {
         this.allactivities = response.activities;
         console.log(this.allactivities);
       },
       (error) => {
-        console.error('Error loading user activities', error);
+        console.error('Error loading all activities', error);
       }
     );
   }
 
-
+  deleteActivity(activityId: string): void {
+    this.restActivityService.deleteActivity(activityId, this.user._id).subscribe(
+      (response) => {
+        console.log('Activity deleted successfully');
+        this.loadAllActivities(); // Reload the activities after deletion
+      },
+      (error) => {
+        console.error('Error deleting activity', error);
+      }
+    );
+  }
 }
